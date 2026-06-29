568a550eac4fa2d2603be1263975b3a2a8914549
yato-developer <developer.yato@gmail.com>
2026-06-29 11:54:23 +0900

    update: Supabaseの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:machamp/src/router/router.dart';
+import 'package:machamp/env/env.dart';
 import 'package:machamp/src/core/constants/app_theme.dart';
+import 'package:machamp/src/router/router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+    url: Env.supabaseUrlDev,
+    publishableKey: Env.supabasePublishableDev,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
