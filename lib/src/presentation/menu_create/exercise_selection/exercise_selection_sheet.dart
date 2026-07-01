b5c199ed1764878f3d0cbf60227a825d8605e4ad
yato-developer <developer.yato@gmail.com>
2026-07-01 17:16:17 +0900

    fix: AppColorsのalpha値を設定するメソッドを使用して色の透明度を修正

--- lib/src/presentation/menu_create/exercise_selection/exercise_selection_sheet.dart
+++ lib/src/presentation/menu_create/exercise_selection/exercise_selection_sheet.dart
@@ -142,7 +142,7 @@ class ExerciseSelectionSheet extends HookConsumerWidget {
                                     width: 40,
                                     height: 40,
                                     decoration: BoxDecoration(
-                                      color: AppColors.purple.withOpacity(0.2),
+                                      color: AppColors.purple.withValues(alpha: 0.2),
                                       borderRadius: BorderRadius.circular(6),
                                     ),
                                     child: const Icon(
